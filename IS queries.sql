f609999c5aef627990c47849b51040c2df1f7e18
RaunakMane13 <158179622+RaunakMane13@users.noreply.github.com>
2024-02-20 09:24:38 -0500

    Add files via upload

--- IS queries.sql
+++ IS queries.sql
@@ -33,47 +33,38 @@ SET GLOBAL net_read_timeout = 120;
 SET GLOBAL net_write_timeout = 120;
 
 -- Load data in table.
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\merged_data_cleaned_withfk.csv'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\merged_data_cleaned.csv'
 INTO TABLE youtube_trending_data
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (video_id, title, publishedAt, channelId, channelTitle, categoryId, trending_date, tags, view_count, likes, dislikes, comment_count, thumbnail_link, comments_disabled, ratings_disabled, description, Region, region_fk); 
--- (2 min 14.63 sec) for data load.
 
 -- Alter publishedAt and trending_date column to date type.
 ALTER TABLE youtube_trending_data
 MODIFY COLUMN publishedAt DATE;
 
 ALTER TABLE youtube_trending_data
-MODIFY COLUMN trending_date DATE;
-
-ALTER TABLE youtube_trending_data
-MODIFY COLUMN thumbnail_link BLOB;
+MODIFY COLUMN trending_data DATE;
 
 -- Create table region_coordinates.
 DROP TABLE IF EXISTS region_coordinates;
 CREATE TABLE region_coordinates (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    latitude DECIMAL(9,6),
-    longitude DECIMAL(9,6),
+    geo_lat DECIMAL(9,6),
+    geo_long DECIMAL(9,6),
     region VARCHAR(2)
 );
 
 -- Add data in region_coordinates table.
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\merged_region_coordinates.csv'
-INTO TABLE region_coordinates
+INTO TABLE youtube_trending_data
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (latitude, longitude, region);
-
--- Add foreign key constraints after adding data.
-ALTER TABLE youtube_trending_data
-ADD CONSTRAINT fk_region_coordinates
-FOREIGN KEY (region_fk) REFERENCES region_coordinates(id);
 
 -- Drop region column to remove redundancy. 
 ALTER TABLE youtube_trending_data
@@ -84,24 +75,14 @@ SELECT Count(*) FROM youtube_trending_data
 JOIN region_coordinates ON youtube_trending_data.region_fk = region_coordinates.id
 WHERE region_coordinates.region = 'IN';
 
-SELECT * FROM youtube_trending_data yt
+SELECT COUNT(*) FROM youtube_trending_data yt
 INNER JOIN region_coordinates rc ON yt.region_fk = rc.id
 WHERE rc.region = 'IN';
 
--- Create index for frequently queried columns to optimize them.
+-- Create index to optimize queries
 CREATE INDEX idx_region_fk ON youtube_trending_data(region_fk);
 CREATE INDEX idx_id ON region_coordinates(id);
 CREATE INDEX idx_region ON region_coordinates(region);
-
-ALTER TABLE youtube_trending_data
-DROP INDEX idx_region_fk;
-
-ALTER TABLE region_coordinates
-DROP INDEX idx_region;
-
-ALTER TABLE region_coordinates
-DROP INDEX idx_id;
-SHOW CREATE TABLE youtube_trending_data;
 
 -- Queries to find non-ascii containing records
 SELECT * FROM youtube_trending_data
@@ -140,26 +121,35 @@ SELECT *
 FROM youtube_trending_data
 WHERE has_non_ascii = TRUE;
 
-SELECT COUNT(*),region from region_coordinates
-group by region;
-
-SELECT MIN(categoryId) AS MinValue, MAX(categoryId) AS MaxV FROM youtube_trending_data;
-
--- Create category table.
+-- Create category table
 DROP TABLE IF EXISTS category;
 CREATE TABLE category (
     id INT PRIMARY KEY,
-    title VARCHAR(255)
+	title VARCHAR(30)
 );
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\merged_category_cleaned.csv'
-INTO TABLE category
+
+-- Add data in region_coordinates table.
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\merged_region_coordinates.csv'
+INTO TABLE youtube_trending_data
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(id, title);
+(id,title);
 
--- add foreign key constraints after adding data.
-ALTER TABLE youtube_trending_data
-ADD FOREIGN KEY (categoryId) REFERENCES category(id);
---  (2 min 7.85 sec) time taken.
+-- geospatial queries to find records within a range.
+SELECT id, region, ST_Distance_Sphere( POINT(longitude, latitude), POINT(-122.4194, 37.7749)) AS distance_in_meters
+FROM region_coordinates
+HAVING distance_in_meters <= 10000
+ORDER BY distance_in_meters;
+
+SELECT COUNT(yt.id) number_of_trending_v, c.title
+FROM youtube_trending_data yt JOIN region_coordinates r ON yt.region_fk = r.id JOIN category c ON c.id = yt.categoryId
+WHERE ST_Distance_Sphere( POINT(r.longitude, r.latitude), POINT(-122.4194, 37.7749)) <= 10000
+GROUP BY c.title
+ORDER BY number_of_trending_v DESC;
+
+SELECT yt.video_id, yt.title, rc.region, rc.latitude, rc.longitude
+FROM youtube_trending_data AS yt
+JOIN region_coordinates AS rc ON yt.region_fk = rc.id
+WHERE rc.latitude BETWEEN 30.0 AND 60.0 AND rc.longitude BETWEEN -130.0 AND -100.0 AND rc.region = 'CA';
